d009b8f446f5984058159fb98349e53d28b13087
shuoshan <ss17564@nyu.edu>
2023-10-11 15:40:23 -0400

    array function

--- classes/sketch.js
+++ classes/sketch.js
@@ -1,34 +1,47 @@
-let circles = [];
-let numCircles = 16;
+let asteroidArray = [ ];
+let maxAsteroids = 16;
+
+class Asteroid{
+  constructor(){
+    this.x = 0;
+    this.y = random(30, height - 30);
+    this.v = random(2, 4);
+    this.r = random(15, 40);
+  }
+
+  update(){
+    this.x += this.v;
+
+    // if reset is needed: pick random diameter and reset x
+    if (this.x > width + this.r) {
+      this.r = random(15, 40);
+      this.x = 0 - this.r;
+    }
+  }
+
+  draw() {
+    fill(220, 10, 100);
+    // draw asteroids
+    ellipse(this.x, this.y, 2 * this.r, 2 * this.r);
+  }
+}
 
 function setup() {
   createCanvas(800, 600);
-  for (let i = 0; i < numCircles; i++) {
-    let myObject = {
-      x: 0,
-      y: random(30, height - 30),
-      v: random(2, 4),
-      r: random(15, 40),
-    };
-    circles.push(myObject);
+  for (let i = 0; i < maxAsteroids; i++) {
+    let anAsteroid = new Asteroid();
+    asteroidArray.push(anAsteroid);
   }
 }
 
 function draw() {
   background("lightblue");
-  fill(220, 10, 100);
 
-  for (let i = 0; i < circles.length; i += 1) {
-    let myObject = circles[i];
+  for (let i = 0; i < asteroidArray.length; i += 1) {
+    let anAsteroid = asteroidArray[i];
+    anAsteroid.update();
+    anAsteroid.draw();
 
-    // draw circles
-    ellipse(myObject.x, myObject.y, 2 * myObject.r, 2 * myObject.r);
-    myObject.x += myObject.v;
-
-    // if reset is needed: pick random diameter and reset x
-    if (myObject.x > width + myObject.r) {
-      myObject.r = random(15, 40);
-      myObject.x = 0 - myObject.r;
-    }
+    
   }
 }
