bae0de45bf926d42d004974eaca93312f2669daa
shuoshan <ss17564@nyu.edu>
2023-10-11 16:08:50 -0400

    another class function

--- classes/sketch.js
+++ classes/sketch.js
@@ -1,5 +1,3 @@
-let asteroidArray = [ ];
-let maxAsteroids = 16;
 
 class Asteroid{
   constructor(){
@@ -26,12 +24,48 @@ class Asteroid{
   }
 }
 
+class Spaceship {
+  constructor(_color) {
+    this.x = random(0, width);
+    this.y = random(0, height);
+    this.vx = random(1, 2);
+    this.vy = random(1, 2);
+    this.color = _color;
+  }
+
+  update() {
+    this.x += this.vx;
+    this.y += this.vy;
+    
+    if (this.x > width){
+      this.x -= width;
+    }
+    if (this.y > height){
+      this.y -= height;
+    }
+  }
+
+  draw() {
+    noStroke();
+    fill(this.color);
+    rect(this.x, this.y, 50, 15);
+  }
+}
+
+let asteroidArray = [ ];
+let maxAsteroids = 16;
+
+let spaceship0;
+let spaceship1;
+
 function setup() {
   createCanvas(800, 600);
   for (let i = 0; i < maxAsteroids; i++) {
     let anAsteroid = new Asteroid();
     asteroidArray.push(anAsteroid);
   }
+  spaceship0 = new Spaceship("gold");
+  spaceship1 = new Spaceship("darkedred");
 }
 
 function draw() {
@@ -41,7 +75,12 @@ function draw() {
     let anAsteroid = asteroidArray[i];
     anAsteroid.update();
     anAsteroid.draw();
-
-    
   }
+  spaceship0.update();
+  spaceship1.update();
+
+  spaceship0.draw();
+  spaceship1.draw();
 }
+
+
